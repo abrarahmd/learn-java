3b6746a324abf97430248cd0893b531d7d80133d
Abrar Ahmed <abrargroad2000@gmail.com>
2025-08-25 16:00:09 +0600

    Strings

---
+++ basics/strings.java
@@ -0,0 +1,10 @@
+
+public class Strings {
+    public static void main(String[] args) {
+        String myName = "My name is " + "\"Abrar\"!!";
+        System.out.println(myName);
+        System.out.println(myName.endsWith("!!"));
+        System.out.println(myName.indexOf("a"));
+        
+    }
+}
